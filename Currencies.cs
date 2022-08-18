15c317be5cd9de8bed6a42ea44f5b7e717baaf25
rita <ritavlas@yandex.ru>
2022-08-18 14:08:21 +0300

    Добавьте файлы проекта.

---
+++ Currencies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    enum Currencies
+    {  USD, //united states dollar
+       EUR, // EURo
+       GBP,//Great Britain Pound
+       JPY,//JaPanese Yen
+       CHF,//Confederation Helvetica Franc
+       CAD,//CAnadian Dollar
+       AUD,//AUstralian Dollar
+       NZD//New Zealand Dollar
+    }
+}
